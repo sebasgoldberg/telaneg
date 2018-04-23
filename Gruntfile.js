dbf87e08982462803692d749096b948b1597689b
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-04-23 15:33:27 -0300

    Fix dist resources.

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
 				options: {
 					resources: {
 						cwd: '<%= dir.webapp %>',
-						prefix: 'simplifique.telaneg'
+						prefix: 'simplifique/telaneg'
 					},
 					dest: '<%= dir.dist %>'
 				},
