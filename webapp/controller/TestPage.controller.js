4e61d7132b13a3882e637c1d1b322998cb772e74
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-05-17 17:54:50 -0300

    Implementação de control para navegar entre passos: NavStep

--- webapp/controller/TestPage.controller.js
+++ webapp/controller/TestPage.controller.js
@@ -1,5 +1,22 @@
 import Controller from 'sap/ui/core/mvc/Controller';
 
 export default Controller.extend("simplifique.telaneg.controller.TestPage", {
+
+    onNext: function(){
+        this.getView().byId('navSteps').nextStep();
+        },
+
+    onPrevious: function(){
+        this.getView().byId('navSteps').previousStep();
+        },
+
+    onEnteringLastStep: function() {
+        console.log('Entering last step.');
+    },
+
+    onLeavingLastStep: function() {
+        console.log('Leaving last step.');
+    },
+
     });
 
